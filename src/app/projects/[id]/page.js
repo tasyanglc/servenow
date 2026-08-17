1cc8ac695e6c32371df6a0caf70f6264ab61e742
tasyanglc <tasyanglch405@gmail.com>
2026-08-17 16:36:53 +0700

    feat: redesign project detail workspace

--- src/app/projects/[id]/page.js
+++ src/app/projects/[id]/page.js
@@ -1,1 +1,9 @@
-'use client'; import { use } from 'react'; import DashboardLayout from '../../../components/DashboardLayout'; import { ProjectDetail } from '../../../components/operations/OperationalViews'; export default function Page({params}){ return <DashboardLayout><ProjectDetail id={use(params).id}/></DashboardLayout>; }
+'use client';
+
+import { use } from 'react';
+import DashboardLayout from '../../../components/DashboardLayout';
+import ProjectDetailWorkspace from '../../../components/operations/ProjectDetailWorkspace';
+
+export default function Page({ params }) {
+  return <DashboardLayout><ProjectDetailWorkspace id={use(params).id} /></DashboardLayout>;
+}
